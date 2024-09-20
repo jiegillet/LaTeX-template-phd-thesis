e041c6cc10d71cd57f067ad5fb9f5725f159281e
Jeremie Gillet <jie.gillet@gmail.com>
2024-09-20 13:22:16 +0900

    add list of publications

--- generate_examples.sh
+++ generate_examples.sh
@@ -5,12 +5,15 @@ set -euox pipefail
 cp oist_thesis.cls Examples/oist_thesis.cls
 cd Examples
 mv Thesis.tex Backup_Thesis.tex
-rm -f Thesis.* # remove auxillary files
+rm -f Thesis.* Pub.* # remove auxillary files
 cp Backup_Thesis.tex Thesis.tex
 
 # Thesis final version
 pdflatex Thesis.tex
-bibtex Thesis.aux
+for aux_file in *.aux
+do
+  bibtex $aux_file
+done
 pdflatex Thesis.tex
 pdflatex Thesis.tex
 mv Thesis.pdf Thesis_final_version.pdf
@@ -18,12 +21,10 @@ mv Thesis.pdf Thesis_final_version.pdf
 # Thesis temporary version
 sed -i 's/\\documentclass\[final\]/\\documentclass\[temporary\]/' Thesis.tex
 pdflatex Thesis.tex
-bibtex Thesis.aux
-pdflatex Thesis.tex
 pdflatex Thesis.tex
 mv Thesis.pdf Thesis_temporary_version.pdf
 
 # cleanup
-rm Thesis.*
+rm -f Thesis.* Pub.*
 mv Backup_Thesis.tex Thesis.tex
 cd ..
